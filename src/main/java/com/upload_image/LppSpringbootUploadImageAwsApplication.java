6e9538e1ec36813eadcd76fc53048d5ab91141f5
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-06-11 17:45:03 -0300

    first commit

---
+++ src/main/java/com/upload_image/LppSpringbootUploadImageAwsApplication.java
@@ -0,0 +1,13 @@
+package com.upload_image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LppSpringbootUploadImageAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LppSpringbootUploadImageAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upload_image/LppSpringbootUploadImageAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upload_image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LppSpringbootUploadImageAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
